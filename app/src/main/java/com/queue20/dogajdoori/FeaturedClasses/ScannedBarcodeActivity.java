83aa51596d92856fb7178f3bea4a7b4a7c18ff77
Sachin Dev Tomar <tomarsachindev@gmail.com>
2020-05-10 23:53:11 +0530

    uploaded drawable and qr activity class activity class

---
+++ app/src/main/java/com/queue20/dogajdoori/FeaturedClasses/ScannedBarcodeActivity.java
@@ -0,0 +1,38 @@
+package com.queue20.dogajdoori.FeaturedClasses;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.zxing.Result;
+import com.queue20.dogajdoori.Fragments.Page1Fragment;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScannedBarcodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    ZXingScannerView scannerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        scannerView =  new ZXingScannerView(this);
+        setContentView(scannerView);
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Page1Fragment.resultQRTextView.setText(result.getText());
+        onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+}
